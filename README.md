48623ab78d352561c6ac144bb35897b5f96fef27
Droid636 <2022043@utsh.edu.mx>
2025-06-20 18:58:04 -0600

    Subida

---
+++ README.md
@@ -0,0 +1,34 @@
+# Web Scraping del Blog de Angular con Puppeteer
+
+## Descripción
+
+Este proyecto consiste en una práctica de scraping web utilizando la librería **Puppeteer**. El objetivo es automatizar la recolección de datos del sitio oficial del blog de Angular: [https://blog.angular.dev](https://blog.angular.dev), extrayendo información útil como títulos de publicaciones, fechas y enlaces directos.
+
+## Tecnologías utilizadas
+
+- [Node.js](https://nodejs.org/)
+- [Puppeteer](https://pptr.dev/)
+- [json2csv](https://www.npmjs.com/package/json2csv) – para exportar a CSV
+- [xlsx](https://www.npmjs.com/package/xlsx) – para exportar a Excel
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local:
+
+    ```bash
+    git clone https://github.com/tu-usuario/angular-blog-scraper.git
+    cd angular-blog-scraper
+    ```
+
+2. Instala las dependencias necesarias:
+
+    ```bash
+    npm install
+    ```
+
+## Ejecución
+
+Ejecuta el script con el siguiente comando:
+
+```bash
+npm start
---
+++ app.js
@@ -0,0 +1,98 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+import { Parser } from "json2csv";
+import XLSX from "xlsx";
+
+async function obtenerDatosAngularBlog() {
+  //1.- Instanciar navegador
+  const navegador = await puppeteer.launch({
+    headless: false,
+    slowMo: 1000,
+  });
+
+  //2.- Abrir una nueva pestaña en navegador
+  const pagina = await navegador.newPage();
+
+  //3.- Ir a la página web
+  await pagina.goto("https://blog.angular.dev/");
+
+  const datos = await pagina.evaluate(() => {
+    const resultados = [];
+    document
+      .querySelectorAll("main>div:first-child>div:nth-child(2)>div>div.o.q")
+      .forEach((elemento) => {
+        const imagen = elemento.querySelector("img").src;
+        const autor = elemento.querySelector("div>a>p").innerText;
+        const titulo = elemento.querySelector("div>a>h2").innerText;
+        const texto = elemento.querySelector("div>a>div>h3").innerText;
+        const fecha = elemento.querySelector("div>span>div>div").innerText;
+        const fechaModificada = fecha.split(`\n`);
+
+        const data = {
+          articulo: {
+            titulo,
+            texto,
+            autor: {
+              nombre: autor,
+              avatar: imagen,
+            },
+            fecha: fechaModificada[0],
+            reacciones: {
+              likes: fechaModificada[1],
+              comentarios: fechaModificada[2],
+            },
+          },
+        };
+
+        resultados.push(data);
+      });
+    return resultados;
+  });
+
+  // Crear archivo JSON
+  let jsonData = JSON.stringify(datos, null, 2);
+  fs.writeFileSync("angular_blog_articulos.json", jsonData, "utf-8");
+  console.log("Archivo JSON creado exitosamente'");
+
+  // Crear archivo CSV
+  const fields = [
+    "titulo",
+    "texto",
+    "autor.nombre",
+    "autor.avatar",
+    "fecha",
+    "reacciones.likes",
+    "reacciones.comentarios",
+  ];
+  const json2csvParse = new Parser({
+    fields,
+    defaultValue: "No hay Información",
+  });
+  const csv = json2csvParse.parse(datos.map((item) => item.articulo));
+  fs.writeFileSync("angular_blog_articulos.csv", csv, "utf-8");
+  console.log("Archivo CSV creado exitosamente");
+
+  // Crear archivo XLSX
+  const data = datos.map((item) => {
+    return {
+      Titulo: item.articulo.titulo,
+      Texto: item.articulo.texto,
+      Autor: item.articulo.autor.nombre,
+      Avatar: item.articulo.autor.avatar,
+      Fecha: item.articulo.fecha,
+      Likes: item.articulo.reacciones.likes,
+      Comentarios: item.articulo.reacciones.comentarios,
+    };
+  });
+
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Angular Blog");
+
+  XLSX.writeFile(workbook, "angular_blog_articulos.xlsx");
+  console.log("Archivo XLSX creado exitosamente'");
+
+  navegador.close();
+}
+
+obtenerDatosAngularBlog();
